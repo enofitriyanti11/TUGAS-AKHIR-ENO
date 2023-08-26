2a9f662a75270a10faf2b900a4e233bf17e51616
eno fitriyanti <enofitriy4nti@gmail.com>
2023-08-26 13:15:08 +0700

    update

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -12,17 +12,6 @@
                         <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
                         Peminjaman
                     </a>
-                    <!-- <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                        <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                        Transaksi
-                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                    </a>
-                    <div class="nav-content collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                        <nav class="sb-sidenav-menu-nested nav">
-                            <a class="nav-link" href="/peminjaman">Peminjaman</a>
-                            <a class="nav-link" href="/pengembalian">Pengembalian</a>
-                        </nav>
-                    </div> -->
                     <div class="sb-sidenav-menu-heading">MENU</div>
                     <a class="nav-link" href="/anggota">
                         <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
@@ -47,10 +36,6 @@
                         Laporan
                     </a>
                 </div>
-            </div>
-            <div class="sb-sidenav-footer">
-                <div class="small">Logged in as:</div>
-                Start Bootstrap
             </div>
         </nav>
     </div>
